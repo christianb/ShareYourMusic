f1b7791383d17b5ad11bcb76b93a8cc10f07457e
Christian Bunk <christianb.github@googlemail.com>
2011-10-27 14:49:26 +0200

    add user tests

--- src/app/models/user.rb
+++ src/app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ActiveRecord::Base
+  validates :firstname, :lastname, :email, :password, :state, :presence => true  
+  validates :email, :uniqueness => true
 end
--- src/test/unit/user_test.rb
+++ src/test/unit/user_test.rb
@@ -1,7 +1,26 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  test "the truth" do
-     assert false
+  # set up test data for table user
+  fixtures :users
+  
+  test "user attributes must not be empty" do
+     user = User.new
+     assert user.invalid?
+     assert user.errors[:firstname].any?
+     assert user.errors[:lastname].any?
+     assert user.errors[:email].any?
+     assert user.errors[:password].any?
+     assert user.errors[:state].any?
+  end
+  
+  test "user email must be unique" do
+    user = User.new(:email => users(:one).email,
+                    :firstname => "Martin",
+                    :lastname => "Schmidt",
+                    :password => "23456",
+                    :state => "active")
+    assert !user.save
+    assert_equal I18n.translate('activerecord.errors.messages.taken'), user.errors[:email].join('; ')
   end
 end
