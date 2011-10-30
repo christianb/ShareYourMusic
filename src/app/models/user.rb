af46c2d974f08811902f6bdd85345fc1025422cf
Christian Bunk <christianb.github@googlemail.com>
2011-10-30 18:03:19 +0100

    add unit tests for user

--- src/app/models/user.rb
+++ src/app/models/user.rb
@@ -3,4 +3,11 @@ class User < ActiveRecord::Base
   validates :email, :uniqueness => true
   validates :email, :format => {:with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i}
   validates :image_uri, :format => {:with => %r{\.(jpg|png)$}i}
+  
+  # each user is assigned to exactly one role
+  belongs_to :role
+  
+  # check if the role record referenced by the foreign key exist
+  validates_presence_of :role
+  #has_many :compact_disks
 end
--- src/test/unit/user_test.rb
+++ src/test/unit/user_test.rb
@@ -2,7 +2,15 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   # set up test data for table user
-  fixtures :users
+  fixtures :roles, :users
+  
+  def setup
+    @user = users(:peter)
+  end
+  
+  def teardown
+    @user = nil
+  end
   
   test "user attributes must not be empty" do
      user = User.new
@@ -15,66 +23,51 @@ class UserTest < ActiveSupport::TestCase
   end
   
   test "user email must be unique" do
-    user = User.new(:email => users(:one).email,
-                    :firstname => users(:one).firstname,
-                    :lastname => users(:one).lastname,
-                    :password => users(:one).password,
-                    :state => users(:one).state)
+    user = User.new(:email => users(:peter).email,
+                    :firstname => "vorname",
+                    :lastname => "nachname",
+                    :password => "abcde",
+                    :state => "active")
     assert !user.save
     assert_equal I18n.translate('activerecord.errors.messages.taken'), user.errors[:email].join('; ')
   end
   
-  test "invalid image uri" do
-    user = User.new(:email => "test@test.com",
-                    :firstname => users(:one).firstname,
-                    :lastname => users(:one).lastname,
-                    :password => users(:one).password,
-                    :state => users(:one).state,
-                    :image_uri => "image.sh")
-    assert user.invalid?
-  end
-  
-  test "valid image uri" do
-    user = User.new(:email => "test@test.com",
-                    :firstname => users(:one).firstname,
-                    :lastname => users(:one).lastname,
-                    :password => users(:one).password,
-                    :state => users(:one).state,
-                    :image_uri => "image.png")
-    assert user.valid?
-  end
-  
-  test "valid email pattern" do
-    user = User.new(:firstname => users(:one).firstname,
-                    :lastname => users(:one).lastname,
-                    :email => "hans.peter@google.com",
-                    :password => users(:one).password,
-                    :state => users(:one).state,
-                    :image_uri => users(:one).image_uri)
-    assert user.valid?
-  end
-  
-  test "invalid email pattern" do
-    user = User.new(:firstname => users(:one).firstname,
-                    :lastname => users(:one).lastname,
-                    :password => users(:one).password,
-                    :state => users(:one).state,
-                    :image_uri => users(:one).image_uri)
+  test "image uri" do
+    @user.image_uri = "image.sh"
+    assert @user.invalid?, "image uri should be invalid"
     
-    user.email = "@me.com"
-    assert user.invalid?, "Should fail due starts with an @ sign"
+    @user.image_uri = "image.png"
+    assert @user.valid?, "image uri should be valid"
+  end
+  
+  test "email pattern" do
+    @user.email = "name@domain.com"
+    assert @user.valid?, "email pattern should be valid"
+    
+    @user.email = "@me.com"
+    assert @user.invalid?, "Should fail due starts with an @ sign"
                     
-    user.email = "testme.com"
-    assert user.invalid?, "Should fail due to missing @ sign"
+    @user.email = "testme.com"
+    assert @user.invalid?, "Should fail due to missing @ sign"
     
-    user.email = "test@mecom"
-    assert user.invalid?, "Should fail due to missing . sign in domain part"
+    @user.email = "test@mecom"
+    assert @user.invalid?, "Should fail due to missing . sign in domain part"
     
-    user.email = "test@me."
-    assert user.invalid?, "Should fail due no top-level domain is given"
+    @user.email = "test@me."
+    assert @user.invalid?, "Should fail due no top-level domain is given"
     
-    user.email = "test@"
-    assert user.invalid?, "Should fail due no domain is given"
-    
+    @user.email = "test@"
+    assert @user.invalid?, "Should fail due no domain is given"
   end
+  
+  test "role id must exist to create new user" do
+    @user.email = "abc@htw.de"
+    @user.role_id = 0
+    assert @user.save, "Should save user due role_id exist"
+    
+    @user.email = "abcd@htw.de"
+    @user.role_id = 42
+    assert !@user.save, "should not save user due role_id does not exist"
+  end
+  
 end
