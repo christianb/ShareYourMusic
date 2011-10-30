d0b3a91fcea7d738a96a9b711d28187f7797071f
Christian Bunk <christianb.github@googlemail.com>
2011-10-30 22:45:11 +0100

    add unit test for transaction

--- src/app/models/transaction.rb
+++ src/app/models/transaction.rb
@@ -1,2 +1,34 @@
-class Transaction < ActiveRecord::Base
+class UserIDsValidator < ActiveModel::Validator
+  def validate(record)
+    if record.provider_id == record.receiver_id
+      record.errors[:base] << "same id of provider and receiver is not allowed"
+    end
+  end
 end
+
+class UserPresenceValidator < ActiveModel::Validator
+  def validate(record)
+    user = User.where(:id => record.provider_id).first
+    if user.nil?
+      record.errors[:base] << "Provider id does not exist"
+    end
+    
+    user = User.where(:id => record.receiver_id).first
+    if user.nil?
+      record.errors[:base] << "Reiceiver id does not exist"
+    end
+  end
+end
+
+class Transaction < ActiveRecord::Base
+  belongs_to :user, :class_name => "User", :foreign_key => "provider_id"
+  belongs_to :user, :class_name => "User", :foreign_key => "receiver_id"
+  
+  validates_with UserIDsValidator
+  validates_with UserPresenceValidator
+  
+end
+
+
+
+
--- src/test/unit/transaction_test.rb
+++ src/test/unit/transaction_test.rb
@@ -1,7 +1,31 @@
 require 'test_helper'
 
 class TransactionTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  fixtures :users
+  
+  def setup
+    @user = users(:peter)
+  end
+  
+  def teardowns
+    @user = nil
+  end
+  
+  test "user id's must exist to create new transaction" do    
+    transaction = Transaction.new(:provider_id => 0,
+                                  :receiver_id => 0)
+    assert !transaction.save, "should not be saved due same user id"
+    
+    transaction.provider_id = 1
+    assert transaction.save, "should be saved du both user id's exist"
+    
+    transaction = Transaction.new(:provider_id => 0,
+                                  :receiver_id => 2)
+    assert !transaction.save, "should not be saved du user id 2 does not exist"
+    
+    transaction = Transaction.new(:provider_id => 2,
+                                  :receiver_id => 0)
+    assert !transaction.save, "should not be saved du user id 2 does not exist"
+    
+  end
 end
