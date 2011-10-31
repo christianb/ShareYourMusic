e735aece0facf862b8e6dc428e60e93fcadc5994
Christian Bunk <christianb.github@googlemail.com>
2011-10-31 13:13:09 +0100

    add unit test for role

--- src/app/models/role.rb
+++ src/app/models/role.rb
@@ -1,3 +1,6 @@
 class Role < ActiveRecord::Base
+  validates :role, :presence => true
+  validates :role, :uniqueness => true
+  
   has_many :user
 end
--- src/test/unit/role_test.rb
+++ src/test/unit/role_test.rb
@@ -1,7 +1,22 @@
 require 'test_helper'
 
 class RoleTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+  end
+  
+  def teardown
+  end
+  
+  test "role name must exist" do
+    role = Role.new
+    assert !role.save, "should not be saved due role name is empty"
+  end
+  
+  test "role must unique" do
+    role = Role.new(:role => "system")
+    assert role.save, "should be saved due role does not exist"
+    
+    role = Role.new(:role => "system")
+    assert !role.save, "should not be saved due role does exist"
+  end
 end
