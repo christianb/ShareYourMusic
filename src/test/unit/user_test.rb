9ab98d1053d2cb0abc06e3f0511a38a2565fbc18
Christian Bunk <christianb.github@googlemail.com>
2011-11-05 23:08:18 +0100

    add image_uri test, but stil fails
    image_uri can be nil, see #38

--- src/test/unit/user_test.rb
+++ src/test/unit/user_test.rb
@@ -22,7 +22,12 @@ class UserTest < ActiveSupport::TestCase
      assert user.errors[:state].any?
   end
   
-  test "user email must be unique" do
+  test "email" do
+    @user.email = nil
+    assert @user.invalid?, "email must exist"
+  end
+  
+  test "user email must be unique" do  
     user = User.new(:email => users(:peter).email,
                     :firstname => "vorname",
                     :lastname => "nachname",
@@ -38,6 +43,11 @@ class UserTest < ActiveSupport::TestCase
     
     @user.image_uri = "image.png"
     assert @user.valid?, "image uri should be valid"
+    
+    # FAILS
+    # FIX TEST !!!
+    @user.image_uri = nil
+    assert @user.valid?, "image uri is allowed to be nil"
   end
   
   test "email pattern" do
