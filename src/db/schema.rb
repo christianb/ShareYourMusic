76479c922ace83b76328f147abac6b0611db6912
Alexander Miller <alexandermiller@ubuntu.(none)>
2011-11-10 16:36:24 +0100

    Simple-private-messaging Plug-In integrated

--- src/db/schema.rb
+++ src/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111109101927) do
+ActiveRecord::Schema.define(:version => 20111110152245) do
 
   create_table "compact_disks", :force => true do |t|
     t.integer  "user_id",     :null => false
@@ -22,6 +22,18 @@ ActiveRecord::Schema.define(:version => 20111109101927) do
     t.string   "image_uri"
     t.text     "description"
     t.boolean  "visible"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "messages", :force => true do |t|
+    t.integer  "sender_id"
+    t.integer  "recipient_id"
+    t.boolean  "sender_deleted",    :default => false
+    t.boolean  "recipient_deleted", :default => false
+    t.string   "subject"
+    t.text     "body"
+    t.datetime "read_at"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
