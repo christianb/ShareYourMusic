d39f9509672dbbaa74cf0770420db127f27e49d8
Christian Sandvoß <christian.sandvoss@gmail.com>
2011-11-10 13:26:19 +0100

    edit seed.rb

--- src/db/seeds.rb
+++ src/db/seeds.rb
@@ -6,9 +6,14 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-puts 'Create CD'
-CompactDisk.create! :user_id => 1, :title => "13", :artist=> "Die Aerzte", :genre => "Punk"
-
 puts 'Create Roles'
 Role.create! :role => "user"
 Role.create! :role => "admin"
+
+puts 'Create User'
+User.create! :role_id => 1, :lastname => "User", :firstname=> "Ein", :email => "user@user.de", :image_uri => "user.png"
+User.create! :role_id => 2, :lastname => "Admin", :firstname=> "Ein", :email => "admin@admin.de", :image_uri => "admin.png"
+
+puts 'Create CD'
+CompactDisk.create! :user_id => 1, :title => "13", :artist=> "Die Aerzte", :genre => "Punk"
+
