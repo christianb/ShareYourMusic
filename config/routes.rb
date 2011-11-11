358cd85533a207113561bc033518d182e925b5ef
Christian Sandvoß <christian.sandvoss@gmail.com>
2011-11-11 18:29:43 +0100

    change links in show.html.erb to devise actions

--- config/routes.rb
+++ config/routes.rb
@@ -10,10 +10,10 @@ Projekt::Application.routes.draw do
   # Sample of named route:
   #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
   # This route can be invoked with purchase_url(:id => product.id)
-
+  
   # Sample resource route (maps HTTP verbs to controller actions automatically):
   resources :compact_disk
-     resources :user
+  resources :user
 
   # Sample resource route with options:
   #   resources :products do
