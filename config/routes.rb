08be14e84585f83e5e29570554efe9dbe44277ab
Christian Sandvoß <christian.sandvoss@gmail.com>
2011-11-11 15:56:58 +0100

    add welcome screen

--- config/routes.rb
+++ config/routes.rb
@@ -50,7 +50,7 @@ Projekt::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-   root :to => 'CompactDisk#index'
+   root :to => 'welcome#index'
 
   # See how all your routes lay out with "rake routes"
 
