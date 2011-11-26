6a46401fc9a7c34b6e659128f815021edd60a498
Christian Sandvoß <christian.sandvoss@gmail.com>
2011-11-26 17:07:48 +0100

    add textfields for songs in compact_disk new view

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,7 @@ $(document).ready(function(){
 	//selectCD();
 	dragDropCD();
 	//openInBox();
+	addSongField();
 });
 
 function selectCD(){
@@ -68,3 +69,31 @@ function openInBox(){
 	});
 }
 */
+
+function addSongField(){
+	$('#btnAdd').click(function() {
+                var num     = $('.clonedInput').length; 
+                var newNum  = new Number(num + 1);      
+ 
+                var newElem = $('#input' + num).clone().attr('id', 'input' + newNum);
+ 
+                newElem.children(':first').attr('id', 'name' + newNum).attr('name', 'name' + newNum);
+ 
+                $('#input' + num).after(newElem);
+ 
+                $('#btnDel').attr('disabled','');
+			
+ 			});
+               
+            $('#btnDel').click(function() {
+                var num = $('.clonedInput').length;
+                $('#input' + num).remove();
+ 
+                $('#btnAdd').attr('disabled','');
+ 
+                if (num-1 == 1)
+                    $('#btnDel').attr('disabled','disabled');
+            });
+ 
+            $('#btnDel').attr('disabled','disabled');
+}
