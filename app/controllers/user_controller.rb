73f4ac291b26ce9c7b910c23c2effded6a67c29a
Christian Bunk <christianb.github@googlemail.com>
2011-12-23 20:17:35 +0100

    modify layout

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -4,11 +4,7 @@
   # just print some attributes of a user
   # GET /user/1
   def show
-    if !user_signed_in?
       @user = User.find(params[:id]);
-    else
-      @user = current_user
-    end
   end  
   
   def reset_password
