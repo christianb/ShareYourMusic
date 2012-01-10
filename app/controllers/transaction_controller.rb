2ab17d6b41c9c900448696a422d1e7a2d8f7fbc3
Christian Bunk <christianb.github@googlemail.com>
2012-01-10 20:53:53 +0100

    change order

--- app/controllers/transaction_controller.rb
+++ app/controllers/transaction_controller.rb
@@ -534,6 +534,6 @@ def modifyRejected
 end
 
 def history
-  @transactions = Transaction.where("provider_id = ? OR receiver_id = ?", current_user.id, current_user.id).paginate(:page => params[:page], :per_page => 10)
+  @transactions = Transaction.where("provider_id = ? OR receiver_id = ?", current_user.id, current_user.id).order("created_at DESC").paginate(:page => params[:page], :per_page => 10)
 end
 end
